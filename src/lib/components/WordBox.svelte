9ad597583e933d3c07d6b12de40ddf8590dd8b92
almostahexagon2 <almostahexagon@proton.me>
2025-12-07 20:39:29 +0000

    optional kanji/vimsonjo

--- src/lib/components/WordBox.svelte
+++ src/lib/components/WordBox.svelte
@@ -16,8 +16,9 @@
 		class="text-xl hocus-visible:text-blue-500 transition"
 	>
 		<span class="font-bold">{word.word}</span>
-		<span class="ml-1 faded">{word.vimsonjo}</span>
-		<span class="ml-1 faded">{word.kanji}</span>
+		
+		{#if word.vimsonjo}<span class="ml-1 faded">{word.vimsonjo}</span>{/if}
+		{#if word.kanji}<span class="ml-1 faded">{word.kanji}</span>{if}
 	</a>
 
 	<WordDetails {word} />
